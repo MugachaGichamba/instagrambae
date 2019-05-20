fc52b14c56adfa6a604ac483d15f8fa55060e741
willy_kid <nillyw31@gmail.com>
2019-05-20 15:47:03 +0300

    added login and logout views to urls list paths

--- instagrambae/urls.py
+++ instagrambae/urls.py
@@ -21,4 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
