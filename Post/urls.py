a0faaed58a926928aa3f704411411f0bc792f42e
willy_kid <nillyw31@gmail.com>
2019-05-22 16:05:08 +0300

    created like template and urls

--- Post/urls.py
+++ Post/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('myposts/', views.myPosts, name='myposts'),
-    path('myposts/new', views.PostCreateView.as_view(), name='post-create')
+    path('myposts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('like/', views.like_post, name='like_post')
+
 ]
