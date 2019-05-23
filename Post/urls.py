29d2ea66eba53ef95780cb4a51495bcdd0e7da7a
willy_kid <nillyw31@gmail.com>
2019-05-23 08:59:10 +0300

    created comments model

--- Post/urls.py
+++ Post/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('myposts/', views.myPosts, name='myposts'),
     path('myposts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('like/', views.like_post, name='like_post')
+    path('like/', views.like_post, name='like_post'),
 
 ]
